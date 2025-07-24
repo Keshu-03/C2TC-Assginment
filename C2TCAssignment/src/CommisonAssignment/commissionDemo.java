81284fa3780c9b434e23f836c88974acd781cc09
Sowmiya <Sowmiya@DESKTOP-86E1PE4>
2025-07-24 13:45:54 +0530

    CommissionDemo

---
+++ C2TCAssignment/src/CommisonAssignment/commissionDemo.java
@@ -0,0 +1,18 @@
+package CommisonAssignment;
+public class commissionDemo {
+	public static void main(String[] args) {
+		Commision c=new Commision();
+		c.setName("Shalini");
+		c.setAddress("anna nagar, Chennnai");
+		c.setPhone("8671524361");
+		c.setSalesAmount(80000);
+		
+		c.SaleCommission();
+		
+		
+		
+	}
+
+}
+
+
